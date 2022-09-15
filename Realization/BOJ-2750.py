6f3bcb2a5fd5679794197a0d2c847ba35440f961
김주한 <83435602+juhan2103@users.noreply.github.com>
2022-09-15 23:28:41 +0900

    realization - [BOJ]2750

---
+++ Realization/BOJ-2750.py
@@ -0,0 +1,8 @@
+n = int(input())  # n을 입력받는다
+numbers = []  # 숫자를 입력받을 리스트를 선언
+for i in range(n):  # n만큼 반복
+    num = int(input())  # 숫자를 입력받는다
+    numbers.append(num)  # 입력받은 숫자를 리스트 안에 저장
+numbers.sort()  # 오름차순으로 정렬
+for i in numbers:  # 오름차순으로 출력
+    print(i)
