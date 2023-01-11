b4be38766b2822c176fc0194ab83b27b4bf7356e
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-01-11 22:27:19 +0900

    sort - [BOJ]11651

---
+++ Sort/BOJ-11651.py
@@ -0,0 +1,8 @@
+import sys
+n = int(input())
+num_list = []
+for i in range(n):
+    num_list.append(list(map(int, sys.stdin.readline().split())))
+num_list.sort(key=lambda x: (x[1], x[0]))
+for i in num_list:
+    print(i[0], i[1])
