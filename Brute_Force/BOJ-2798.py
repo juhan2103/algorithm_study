1cdff8f19ba6f3da2a36cc18103c7a497cb65691
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-02-28 22:58:14 +0900

    brute force - [BOJ]2798

---
+++ Brute_Force/BOJ-2798.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+n_list = list(map(int, input().split()))
+result = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if n_list[i] + n_list[j] + n_list[k] > m:
+                continue
+            else:
+                result = max(result, n_list[i] + n_list[j] + n_list[k])
+print(result)
