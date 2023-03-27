ea02cc8a2c1a5b1e718f558351c8a18867768721
김주한 <83435602+juhan2103@users.noreply.github.com>
2023-03-27 20:23:05 +0900

    dfs_bfs - [BOJ]24445

---
+++ DFS_BFS/BOJ-24445.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+def bfs(start):
+  queue = deque([start])
+  visited[start] = 1
+  global count
+  while queue:
+    v = queue.popleft()
+    graph[v].sort(reverse = True)
+    for i in graph[v]:
+      if not visited[i]:
+        count += 1
+        queue.append(i)
+        visited[i] = count
+n, m, v = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+for i in range(m):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+visited = [0] * (n + 1)
+count = 1
+bfs(v)
+for i in visited[1:]:
+  print(i)
